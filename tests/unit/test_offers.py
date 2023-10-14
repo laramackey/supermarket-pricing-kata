550991c6f9cba82d70dc38553c34533d25cdeceb
laramackey <lara.mackey@focusrite.com>
2023-10-14 13:45:14 +0100

    increase test coverage of offers

--- tests/unit/test_offers.py
+++ tests/unit/test_offers.py
@@ -15,16 +15,34 @@ def test_product_catalogue() -> Dict[str, Union[Product, Type[Product]]]:
     }
 
 
-def test_three_for_two_offer(test_product_catalogue):
+def test_singular_three_for_two_offer(test_product_catalogue):
     offer = ThreeForTwo(test_product_catalogue["a"])
-    cart_count = {"a": 5}  # Price 1.0
+    cart_count = {"a": 3}  # Price 1.0
     discount_amount = offer.check_and_apply(cart_count)
-    # Before Discount = 5 * 1.0= 5.0
-    # After Discount = 5.0 - 1.0 = 4.0
+    # Before Discount = 3 * 1.0= 3.0
+    # After Discount = 3.0 - 1.0 = 2.0
     assert discount_amount == 1.0
 
 
-def test_two_for_price_offer(test_product_catalogue):
+def test_multiple_three_for_two_offer(test_product_catalogue):
+    offer = ThreeForTwo(test_product_catalogue["a"])
+    cart_count = {"a": 7}  # Price 1.0
+    discount_amount = offer.check_and_apply(cart_count)
+    # Before Discount = 7 * 1.0= 7.0
+    # After Discount = 7.0 - 2.0 = 5.0
+    assert discount_amount == 2.0
+
+
+def test_singular_two_for_price_offer(test_product_catalogue):
+    offer = TwoForPrice(test_product_catalogue["b"], 1.5)
+    cart_count = {"b": 2}  # Price 1.0
+    discount_amount = offer.check_and_apply(cart_count)
+    # Before Discount = 2 * 1.0 = 2.0
+    # After Discount = 2.0 - 1.5 - 0.5
+    assert discount_amount == 0.5
+
+
+def test_multiple_two_for_price_offer(test_product_catalogue):
     offer = TwoForPrice(test_product_catalogue["b"], 1.5)
     cart_count = {"b": 5}  # Price 1.0
     discount_amount = offer.check_and_apply(cart_count)
@@ -33,7 +51,22 @@ def test_two_for_price_offer(test_product_catalogue):
     assert discount_amount == 1.0
 
 
-def test_three_from_set_offer(test_product_catalogue):
+def test_singular_three_from_set_offer(test_product_catalogue):
+    offer = ThreeFromSetForPrice(
+        [test_product_catalogue["b"], test_product_catalogue["c"], test_product_catalogue["d"]], 3.0, "letters"
+    )
+    cart_count = {
+        "b": 1,  # Price 1.0
+        "c": 1,  # Price 1.1
+        "d": 1,  # Price 1.2
+    }
+    discount_amount = offer.check_and_apply(cart_count)
+    # Before Discount = 3.3
+    # After Discount = 3.3 + 3.0 = 0.3
+    assert discount_amount == 0.3
+
+
+def test_multiple_three_from_set_offer(test_product_catalogue):
     offer = ThreeFromSetForPrice(
         [test_product_catalogue["b"], test_product_catalogue["c"], test_product_catalogue["d"]], 3.0, "letters"
     )
